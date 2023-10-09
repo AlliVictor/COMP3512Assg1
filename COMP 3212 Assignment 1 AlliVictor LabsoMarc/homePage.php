40eb572d87b7922e04700e15a852f4861f1a11d2
AlliVictor <valli978@mtroyal.ca>
2023-10-09 12:59:30 -0600

    fix for previous

--- COMP 3212 Assignment 1 AlliVictor LabsoMarc/homePage.php
+++ COMP 3212 Assignment 1 AlliVictor LabsoMarc/homePage.php
@@ -17,7 +17,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link type="text/css" href="css/home-page.css" rel="stylesheet">
+    <link type="text/css" href="css/homePage.css" rel="stylesheet">
     
 
     <title>Home | Song Bank</title>
