44242aba550d12b27ecaa2cdc4f6d5edcfdc4d6f
AlliVictor <valli978@mtroyal.ca>
2023-10-10 17:06:27 -0600

    filler

--- COMP 3212 Assignment 1 AlliVictor LabsoMarc/singleSongPage.php
+++ COMP 3212 Assignment 1 AlliVictor LabsoMarc/singleSongPage.php
@@ -20,7 +20,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link type="text/css" href="css/searchPage.css" rel="stylesheet">
     <link type="text/css" href="css/homePage.css" rel="stylesheet">
     <link type="text/css" href="css/singleSongPage.css" rel="stylesheet">
 
